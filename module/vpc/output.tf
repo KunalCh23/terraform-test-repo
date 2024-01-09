3fd6a72f3e16aac71816adbc219ccef3e4284e18
Kunal Chaudhary <98645631+KunalCh23@users.noreply.github.com>
2024-01-09 17:01:59 +0530

    Update output.tf

--- module/vpc/output.tf
+++ module/vpc/output.tf
@@ -1,1 +1,15 @@
 
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.vpc.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the created public subnets"
+  value       = aws_subnet.public_subnet[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the created private subnets"
+  value       = aws_subnet.private_subnet[*].id
+}
